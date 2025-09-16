d7295145b1a795a3f461d8f669079eb48e8e5fb4
pjs <bavuchoko@naver.com>
2025-09-16 09:20:37 +0900

    [add] 테스트용 예제

---
+++ example/src/App.tsx
@@ -0,0 +1,14 @@
+import {JsDatePicker } from '@bavuchoko/js-datepicker'
+function App() {
+
+
+
+  return (
+    <div className="App" style={{width:'100%', display:"flex"}}>
+
+    <JsDatePicker />
+    </div>
+  );
+}
+
+export default App;
